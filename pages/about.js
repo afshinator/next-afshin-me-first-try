22e3b6bf061cf21d756af81c4e1ac58532b92889
Afshin Mokhtari <afshinator@hotmail.com>
2020-12-27 22:24:04 -0800

    More words and a picture

--- pages/about.js
+++ pages/about.js
@@ -1,7 +1,16 @@
 import Link from "next/link";
 import Head from "next/head";
 import Layout from "../components/layout";
-import { Avatar, AvatarGroup, Box, Flex, Heading } from "@chakra-ui/react";
+import {
+  Avatar,
+  AvatarGroup,
+  Box,
+  Flex,
+  Heading,
+  Image,
+  Text,
+} from "@chakra-ui/react";
+import App from "./_app";
 
 export async function getStaticProps() {
   const randomClass = "tileImage" + Math.floor(Math.random() * Math.floor(10));
@@ -30,7 +39,14 @@ export default function AboutPage({ randomClass }) {
         <Heading as="h1" size="xl" fontWeight="regular" px={4}>
           About Afsheezy
         </Heading>
-        <Box maxW="4xl" m="0 auto" borderWidth="1px" borderRadius="lg" p={15} my={15}>
+        <Box
+          maxW="4xl"
+          m="0 auto"
+          borderWidth="1px"
+          borderRadius="lg"
+          p={15}
+          my={15}
+        >
           <Heading as="h4" size="2xl" fontWeight="regular" px={4}>
             Technology
           </Heading>
@@ -62,8 +78,103 @@ export default function AboutPage({ randomClass }) {
               <Avatar name="Python" src="/img/tech/python.png" />
             </AvatarGroup>
             <Heading as="h6" size="md" fontWeight="regular" px={4}>
-              But I've been in development so long that I have experience
-              with a variety of technlogies, on and off the web.
+              But I've been in development so long that I have experience with a
+              variety of technlogies, on and off the web.
+            </Heading>
+          </Flex>
+          <Flex maxW="2xl" m="0 auto" mt={10}>
+            <Heading as="h5" size="lg" fontStyle="italic" px={4}>
+              Recent engineering efforts
+            </Heading>
+            <Heading as="h6" size="md" fontWeight="regular" px={4}>
+              I've focused professionaly in the fields of medicine and social
+              good with tech. I was the principal mobile engineer on a
+              mindfulness mobile apps at{" "}
+              <Link href={"https://whil.com"}>
+                <a>whil.com</a>
+              </Link>
+              , and at{" "}
+              <Link href={"https://doc.ai"}>
+                <a>doc.ai</a>
+              </Link>{" "}
+              where our goal was to address disease prevention using AI and ML.
+              Most recently I've been diving into Machine Learning projects
+              around health data and pursuing some ideas about how to leverage
+              that data.
+            </Heading>
+          </Flex>
+          <Flex maxW="2xl"  m="0 auto" mt={10}>
+            <Heading as="h6" size="md" fontWeight="regular" px={4}>
+              I like to learn. The computer industry is ever-changing and
+              education is the key to staying to on top. Recently, life gave us
+              the 😷Covid-19 lemon🍋, I've decided to make lemonade by taking
+              the opportunity to learn the following this year:
+              <Flex direction="row">
+                <Box ml={5} mt={5} fontStyle="italic">
+                  <ul>
+                    <li>Gatsby-JS</li>
+                    <li>Next-JS</li>
+                    <li>React Hooks</li>
+                    <li>Web animations</li>
+                    <li>Mobile animations</li>
+                    <li>TypeScript</li>
+                    <li>a bit of Figma</li>
+                    <li>and more</li>
+                  </ul>
+                </Box>
+                <Box ml={10} flex={1}>
+                  <Box fontStyle="italic">
+                    And on the stack to learn:
+                    <ul>
+                      <li>CSS Grid</li>
+                      <li>Svelte</li>
+                    </ul>
+                  </Box>
+                  <Box w={200}>
+                    <Image
+                      src="/img/jamstack.png"
+                      borderRadius="lg"
+                      alt="JAMstack"
+                      mt={5}
+                    />
+                  </Box>
+                </Box>
+              </Flex>
+            </Heading>
+            <Heading as="h5" size="lg" fontStyle="italic" px={4}>
+              Knowledge is Power
+            </Heading>
+          </Flex>
+        </Box>
+        <Box
+          maxW="4xl"
+          m="0 auto"
+          borderWidth="1px"
+          borderRadius="lg"
+          p={15}
+          my={15}
+        >
+          <Heading as="h4" size="2xl" fontWeight="regular" px={4}>
+            Natural Medicine
+          </Heading>
+        </Box>
+        <Box
+          maxW="4xl"
+          m="0 auto"
+          borderWidth="1px"
+          borderRadius="lg"
+          p={15}
+          my={15}
+        >
+          <Heading as="h4" size="2xl" fontWeight="regular" px={4}>
+            Music & Drums
+          </Heading>
+          <Flex maxW="2xl" m="0 auto" mt={10}>
+            <Heading as="h5" size="lg" fontStyle="italic" px={4}>
+              wham bam!
+            </Heading>
+            <Heading as="h6" size="md" fontWeight="regular" px={4}>
+              Coming Soon!
             </Heading>
           </Flex>
         </Box>
